192dc2220e3082eb1b46982ebf4191d1ab0d19a7
shakti lucifer <shakticse@gmail.com>
2017-03-06 00:07:40 -0500

    add request npm module and make example request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("here");
+  request('http://api.openweathermap.org/data/2.5/weather?q=London,uk', function (error, response, body) {
+    console.log(response);
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+});
+res.render('index', { title: 'Weather sApp' });
+
 });
 
 module.exports = router;
