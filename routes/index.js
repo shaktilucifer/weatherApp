79e56e21675737b038c3849b0cc2a8b614f54ce5
shakti lucifer <shakticse@gmail.com>
2017-03-06 00:20:35 -0500

    remove console statements and add appId

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,9 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("here");
-  request('http://api.openweathermap.org/data/2.5/weather?q=London,uk', function (error, response, body) {
-    console.log(response);
+  request('http://api.openweathermap.org/data/2.5/weather?q=London,us&appid=', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Print the google web page.
+    console.log(body);
   }
 });
 res.render('index', { title: 'Weather sApp' });
