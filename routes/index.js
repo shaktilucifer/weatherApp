5dd8f78bd89ceebc1230b084c29eced7cb14d7d4
shakti lucifer <shakticse@gmail.com>
2017-03-06 21:31:39 -0500

    get json from weather api and display using handlebars template

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var config = require('../config/config.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request('http://api.openweathermap.org/data/2.5/weather?q=London,us&appid=', function (error, response, body) {
+  console.log(config.appid);
+  var jsonData;
+  request('http://api.openweathermap.org/data/2.5/weather?q=London,us&appid='+config.appid, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body);
+      jsonData = body;
   }
+  console.log(jsonData);
+  res.render('index', { title: 'Weather sApp', data: jsonData});
+
 });
-res.render('index', { title: 'Weather sApp' });
 
 });
 
